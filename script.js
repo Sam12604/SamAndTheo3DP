933ed8e4b69f094acf504b3f2374b3b50d53a3ba
Sam12604 <samharris12106@gmail.com>
2021-03-20 22:11:29 +0000

    ok

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-//var csv is the CSV file with headers
-function csvJSON(csv){
+var response
+var api_data
 
-    var lines=csv.split("\n");
-    console.log(lines)
-    var result = [];
-  
-    // NOTE: If your columns contain commas in their values, you'll need
-    // to deal with those before doing the next step 
-    // (you might convert them to &&& or something, then covert them back later)
-    // jsfiddle showing the issue https://jsfiddle.net/
-    var headers=lines[0].split(",");
-  
-    for(var i=1;i<lines.length;i++){
-  
-        var obj = {};
-        var currentline=lines[i].split(",");
-  
-        for(var j=0;j<headers.length;j++){
-            obj[headers[j]] = currentline[j];
-        }
-  
-        result.push(obj);
-  
-    }
-  
-    //return result; //JavaScript object
-    return JSON.stringify(result); //JSON
-  }
+async function getData() {
+	response = await fetch('https://samandtheo3dp.com/redirect.json');
+	api_data = await response.json()
 
-data = csvJSON("redirect.csv")
-console.log(data)
+    const params = new URLSearchParams(window.location.search)
+    data = params.get('l')
+    api_data.get(data)
+	document.getElementById("test1").textContent = api_data;
+}
+
+
+getData()
+
+
+const params = new URLSearchParams(window.location.search)
+data = params.get('l')
