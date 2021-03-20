44293544192c71bc9759873b54c0bdf643b07c48
Sam12604 <samharris12106@gmail.com>
2021-03-20 23:04:20 +0000

    okok

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-var response
-var api_data
-
 async function getData() {
-	response = await fetch('https://samandtheo3dp.com/redirect.json');
-	api_data = await response.json()
+	var response = await fetch('https://samandtheo3dp.com/redirect.json');
+	var api_data = await response.json()
 
     const params = new URLSearchParams(window.location.search)
-    data = params.get('l')
-    api_data.get(data)
-	document.getElementById("test1").textContent = api_data;
+    var data = params.get('l')
+
+    var url_r = ""
+
+
+    for (var i = 0; i < api_data.length; i++){
+        if (api_data[i].id == data){
+            var url_r = api_data[i].url
+            console.log(url_r)
+        }
+        
+        if (url_r == ""){
+            var url_r = "https://shop.SamAndTheo3DP.com"
+        }
+    }
+    location.replace(url_r)
 }
 
-
 getData()
-
-
-const params = new URLSearchParams(window.location.search)
-data = params.get('l')
