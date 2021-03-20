4ea75baf9756e206f09ad2b11361f75b1d486f9b
Sam12604 <samharris12106@gmail.com>
2021-03-20 18:43:38 +0000

    tings

---
+++ script.js
@@ -0,0 +1,32 @@
+//var csv is the CSV file with headers
+function csvJSON(csv){
+
+    var lines=csv.split("\n");
+    console.log(lines)
+    var result = [];
+  
+    // NOTE: If your columns contain commas in their values, you'll need
+    // to deal with those before doing the next step 
+    // (you might convert them to &&& or something, then covert them back later)
+    // jsfiddle showing the issue https://jsfiddle.net/
+    var headers=lines[0].split(",");
+  
+    for(var i=1;i<lines.length;i++){
+  
+        var obj = {};
+        var currentline=lines[i].split(",");
+  
+        for(var j=0;j<headers.length;j++){
+            obj[headers[j]] = currentline[j];
+        }
+  
+        result.push(obj);
+  
+    }
+  
+    //return result; //JavaScript object
+    return JSON.stringify(result); //JSON
+  }
+
+data = csvJSON("redirect.csv")
+console.log(data)
